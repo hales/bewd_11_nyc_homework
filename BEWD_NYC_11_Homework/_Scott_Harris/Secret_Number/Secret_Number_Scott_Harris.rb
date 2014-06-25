0d6f19da5f7cc8b95d69b81f834e2d9289064286
scottmh541 <scottmh541@gmail.com>
2014-06-25 18:01:42 -0400

    Submitting homework for Secret Number

---
+++ BEWD_NYC_11_Homework/_Scott_Harris/Secret_Number/Secret_Number_Scott_Harris.rb
@@ -0,0 +1,9 @@
+puts "What number am I thinking of?"
+guess = gets
+
+if guess == 42
+	puts "Correct! You've discovered the meaning of life."
+else guess != 42
+	puts "WRONG. YOU HAVE LAUCHED ALL OF THE WORLD'S NUCLEAR WEAPONS. CONGRATS."
+end
+
